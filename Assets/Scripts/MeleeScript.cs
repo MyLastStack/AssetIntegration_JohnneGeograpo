354713f0e4c14e1616864258c50f36ccd68393dc
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2023-11-26 21:23:50 -0700

    Update MeleeScript.cs

--- Assets/Scripts/MeleeScript.cs
+++ Assets/Scripts/MeleeScript.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class MeleeScript : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "AIControlled")
